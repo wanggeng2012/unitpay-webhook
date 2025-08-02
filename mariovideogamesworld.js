63c65aa22133b49f1383c82a0dd9443141ad34ef
wanggeng2012 <721742089@qq.com>
2025-08-02 19:45:30 +0800

    Update and rename api/unitpay.js to mariovideogamesworld.js

--- mariovideogamesworld.js
+++ mariovideogamesworld.js
@@ -1,6 +1,6 @@
 
 export default (req, res) => {
-  const secretKey = process.env.UNITPAY_SECRET;
+  const secretKey = process.env.UNITPAY_SECRET; // 修复此行
   console.log('UnitPay回调数据:', req.body);
   res.status(200).send('OK');
 }
